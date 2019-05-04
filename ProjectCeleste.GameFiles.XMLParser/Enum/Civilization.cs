4c5118931e9993a7d7748780449f987a89130741
Ryddag <projectceleste0@gmail.com>
2019-05-04 15:34:59 +0200

    Update Civilization.cs

--- ProjectCeleste.GameFiles.XMLParser/Enum/Civilization.cs
+++ ProjectCeleste.GameFiles.XMLParser/Enum/Civilization.cs
@@ -12,14 +12,14 @@ namespace ProjectCeleste.GameFiles.XMLParser.Enum
     [JsonConverter(typeof(StringEnumConverter))]
     public enum CivilizationEnum
     {
-        [XmlEnum("0")] [EnumMember(Value = "0")] Any = 0,
-        [XmlEnum("1")] [EnumMember(Value = "1")] Greek = 1,
-        [XmlEnum("3")] [EnumMember(Value = "3")] Egypt = 3,
-        [XmlEnum("6")] [EnumMember(Value = "6")] Celt = 6,
-        [XmlEnum("8")] [EnumMember(Value = "8")] Persia = 8,
-        [XmlEnum("22")] [EnumMember(Value = "22")] Babylonian = 22,
-        [XmlEnum("24")] [EnumMember(Value = "24")] Norse = 24,
-        [XmlEnum("11")] [EnumMember(Value = "11")] Roman = 11
+        [XmlEnum("0")] [EnumMember(Value = "Any")] Any = 0,
+        [XmlEnum("1")] [EnumMember(Value = "Greek")] Greek = 1,
+        [XmlEnum("3")] [EnumMember(Value = "Egypt")] Egypt = 3,
+        [XmlEnum("6")] [EnumMember(Value = "Celt")] Celt = 6,
+        [XmlEnum("8")] [EnumMember(Value = "Persia")] Persia = 8,
+        [XmlEnum("22")] [EnumMember(Value = "Babylonian")] Babylonian = 22,
+        [XmlEnum("24")] [EnumMember(Value = "Norse")] Norse = 24,
+        [XmlEnum("11")] [EnumMember(Value = "Roman")] Roman = 11
     }
 
     [JsonConverter(typeof(StringEnumConverter))]
