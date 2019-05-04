f4c583f580717d230461412f85eb8ece1b79db76
Ryddag <projectceleste0@gmail.com>
2019-05-04 17:17:24 +0000

    v1.1.6

--- ProjectCeleste.GameFiles.XMLParser/Properties/AssemblyInfo.cs
+++ ProjectCeleste.GameFiles.XMLParser/Properties/AssemblyInfo.cs
@@ -36,6 +36,6 @@ using System.Runtime.InteropServices;
 //
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
-// [assembly: AssemblyVersion("1.1.5")]
-[assembly: AssemblyVersion("1.1.5")]
-[assembly: AssemblyFileVersion("1.1.5")]
+// [assembly: AssemblyVersion("1.1.6")]
+[assembly: AssemblyVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
