5aa630cf6243ac8db5d5fe5669c63089f843f896
Ryddag <projectceleste0@gmail.com>
2019-06-11 14:34:57 +0200

    Update DictionaryContainer.cs

--- src/Container/DictionaryContainer.cs
+++ src/Container/DictionaryContainer.cs
@@ -11,56 +11,56 @@ using ProjectCeleste.GameFiles.XMLParser.Container.Interface;
 
 namespace ProjectCeleste.GameFiles.XMLParser.Container
 {
-    public class DictionaryContainer<T1, T2> : IDictionaryContainer<T1, T2>
+    public class DictionaryContainer<TKey, TValue> : IDictionaryContainer<TKey, TValue>
     {
-        [XmlIgnore] [JsonIgnore] private readonly IDictionary<T1, T2> _valuesDic;
+        [XmlIgnore] [JsonIgnore] private readonly IDictionary<TKey, TValue> _valuesDic;
 
-        private readonly Func<T2, T1> _keySelector;
+        private readonly Func<TValue, TKey> _keySelector;
 
-        public DictionaryContainer(Func<T2, T1> keySelector)
+        public DictionaryContainer(Func<TValue, TKey> keySelector)
         {
             _keySelector = keySelector;
-            _valuesDic = new Dictionary<T1, T2>();
+            _valuesDic = new Dictionary<TKey, TValue>();
         }
 
-        public DictionaryContainer(IDictionary<T1, T2> values, Func<T2, T1> keySelector)
+        public DictionaryContainer(IDictionary<TKey, TValue> values, Func<TValue, TKey> keySelector)
         {
             _keySelector = keySelector;
-            _valuesDic = new Dictionary<T1, T2>(values);
+            _valuesDic = new Dictionary<TKey, TValue>(values);
         }
 
-        public DictionaryContainer(Func<T2, T1> keySelector, IEqualityComparer<T1> comparer)
+        public DictionaryContainer(Func<TValue, TKey> keySelector, IEqualityComparer<TKey> comparer)
         {
             _keySelector = keySelector;
-            _valuesDic = new Dictionary<T1, T2>(comparer);
+            _valuesDic = new Dictionary<TKey, TValue>(comparer);
         }
 
-        public DictionaryContainer(IDictionary<T1, T2> values, Func<T2, T1> keySelector, IEqualityComparer<T1> comparer)
+        public DictionaryContainer(IDictionary<TKey, TValue> values, Func<TValue, TKey> keySelector, IEqualityComparer<TKey> comparer)
         {
             _keySelector = keySelector;
-            _valuesDic = new Dictionary<T1, T2>(values, comparer);
+            _valuesDic = new Dictionary<TKey, TValue>(values, comparer);
         }
 
-        public DictionaryContainer(IEnumerable<T2> values, Func<T2, T1> keySelector)
+        public DictionaryContainer(IEnumerable<TValue> values, Func<TValue, TKey> keySelector)
         {
             _keySelector = keySelector;
             _valuesDic = values.ToDictionary(keySelector);
         }
 
-        public DictionaryContainer(IEnumerable<T2> values, Func<T2, T1> keySelector, IEqualityComparer<T1> comparer)
+        public DictionaryContainer(IEnumerable<TValue> values, Func<TValue, TKey> keySelector, IEqualityComparer<TKey> comparer)
         {
             _keySelector = keySelector;
             _valuesDic = values.ToDictionary(keySelector, comparer);
         }
 
-        public bool ContainsKey(T1 key)
+        public bool ContainsKey(TKey key)
         {
             return _valuesDic.ContainsKey(key);
         }
 
-        public event EventHandler<T2> OnAdd;
+        public event EventHandler<TValue> OnAdd;
 
-        public bool Add(T2 value)
+        public bool Add(TValue value)
         {
             var key = _keySelector(value);
 
@@ -71,11 +71,11 @@ namespace ProjectCeleste.GameFiles.XMLParser.Container
             return true;
         }
 
-        public event EventHandler<T2> OnRemoved;
+        public event EventHandler<TValue> OnRemoved;
 
-        public bool Remove(T1 key)
+        public bool Remove(TKey key)
         {
-            if (!_valuesDic.TryGetValue(key, out T2 value))
+            if (!_valuesDic.TryGetValue(key, out TValue value))
                 return false;
 
             if (!_valuesDic.Remove(key))
@@ -86,21 +86,21 @@ namespace ProjectCeleste.GameFiles.XMLParser.Container
             return true;
         }
 
-        public event EventHandler<T2> OnUpdated;
+        public event EventHandler<TValue> OnUpdated;
 
-        public bool Update(T2 value)
+        public bool Update(TValue value)
         {
             var keyResult = _keySelector(value);
 
-            if (!_valuesDic.TryGetValue(keyResult, out T2 item))
+            if (!_valuesDic.TryGetValue(keyResult, out TValue item))
                 throw new KeyNotFoundException($"KeyNotFoundException '{keyResult}'");
 
             if (ReferenceEquals(value, item) || Equals(value, item))
                 return false;
 
-            if (string.Equals(JsonConvert.SerializeObject(value, Formatting.None),
-                JsonConvert.SerializeObject(item, Formatting.None), StringComparison.OrdinalIgnoreCase))
-                return false;
+            //if (string.Equals(JsonConvert.SerializeObject(value, Formatting.None),
+            //    JsonConvert.SerializeObject(item, Formatting.None), StringComparison.OrdinalIgnoreCase))
+            //    return false;
 
             _valuesDic[keyResult] = value;
 
@@ -111,7 +111,7 @@ namespace ProjectCeleste.GameFiles.XMLParser.Container
 
         [XmlIgnore]
         [JsonIgnore]
-        public T2 this[T1 key] => _valuesDic.TryGetValue(key, out T2 value)
+        public TValue this[TKey key] => _valuesDic.TryGetValue(key, out TValue value)
             ? value
             : throw new KeyNotFoundException($"KeyNotFoundException '{key}'");
 
@@ -119,29 +119,29 @@ namespace ProjectCeleste.GameFiles.XMLParser.Container
         [JsonIgnore]
         public int Count => _valuesDic.Count;
 
-        public T2 Get(T1 key)
+        public TValue Get(TKey key)
         {
-            return _valuesDic.TryGetValue(key, out T2 value)
+            return _valuesDic.TryGetValue(key, out TValue value)
                 ? value
-                : default(T2);
+                : default(TValue);
         }
 
-        public T2 Get(Func<T2, bool> critera)
+        public TValue Get(Func<TValue, bool> critera)
         {
             return Gets().FirstOrDefault(critera);
         }
 
-        public bool TryGet(T1 key, out T2 value)
+        public bool TryGet(TKey key, out TValue value)
         {
             return _valuesDic.TryGetValue(key, out value);
         }
 
-        public IEnumerable<T2> Gets(Func<T2, bool> critera)
+        public IEnumerable<TValue> Gets(Func<TValue, bool> critera)
         {
             return Gets().Where(critera);
         }
 
-        public IEnumerable<T2> Gets()
+        public IEnumerable<TValue> Gets()
         {
             return _valuesDic.ToArray().Select(p => p.Value);
         }
