e5046f4db4db3345f585627c8ff26fb94fc703fd
Ryddag <projectceleste0@gmail.com>
2019-06-15 01:02:58 +0200

    Update Event.cs

--- src/Enum/Event.cs
+++ src/Enum/Event.cs
@@ -13,7 +13,10 @@ namespace ProjectCeleste.GameFiles.XMLParser.Enum
     public enum EventEnum
     {
         [XmlEnum("None")] [EnumMember(Value = "None")] None = 0,
-        [XmlEnum("Halloween")] [EnumMember(Value = "Halloween")] Halloween = 1,
-        [XmlEnum("Winter")] [EnumMember(Value = "Winter")] Winter = 2
+        [XmlEnum("Autumn")] [EnumMember(Value = "Autumn")] Autumn = 1,
+        [XmlEnum("Halloween")] [EnumMember(Value = "Halloween")] Halloween = 2,
+        [XmlEnum("Winter")] [EnumMember(Value = "Winter")] Winter = 3,
+        [XmlEnum("Spring")] [EnumMember(Value = "Spring")] Spring = 4,
+        [XmlEnum("Summer")] [EnumMember(Value = "Summer")] Summer = 5
     }
 }
