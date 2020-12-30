b2e58f0d4446e49a303c598c863cbb0599641eb1
Martin Storø Nyfløtt <martin_mine@hotmail.com>
2020-12-30 18:46:13 +0100

    Added model classes for tactic files and ReforgeItemBlacklist.xml

---
+++ src/Model/ReforgeItemBlacklist.cs
@@ -0,0 +1,25 @@
+﻿using ProjectCeleste.GameFiles.XMLParser.Helpers;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace ProjectCeleste.GameFiles.XMLParser.Model
+{
+    [XmlRoot(ElementName = "ReforgeBlacklistTrait")]
+    public class ReforgeBlacklistTrait
+    {
+        [XmlAttribute(AttributeName = "name")]
+        public string Name { get; set; }
+    }
+
+    [XmlRoot(ElementName = "ReforgeItemBlacklist")]
+    public class ReforgeItemBlacklist
+    {
+        [XmlElement(ElementName = "ReforgeBlacklistTrait")]
+        public List<ReforgeBlacklistTrait> ReforgeBlacklistTrait { get; set; }
+
+        public static ReforgeItemBlacklist FomXmlFile(string xmlLocation)
+        {
+            return XmlUtils.FromXmlFile<ReforgeItemBlacklist>(xmlLocation);
+        }
+    }
+}
