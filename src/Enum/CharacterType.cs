8bdd85108355bf6b0fd0ff54a22d334412bf2e89
Martin Storø Nyfløtt <martin_mine@hotmail.com>
2022-06-27 22:45:28 +0200

    Added new enum for lvl 40 civ creation

--- src/Enum/CharacterType.cs
+++ src/Enum/CharacterType.cs
@@ -15,6 +15,7 @@ namespace ProjectCeleste.GameFiles.XMLParser.Enum
         [XmlEnum("cCharTypeInvalid")] [EnumMember(Value = "cCharTypeInvalid")] Invalid = 0,
         [XmlEnum("cCharTypeNormal")] [EnumMember(Value = "cCharTypeNormal")] Normal = 1,
         [XmlEnum("cCharTypeSkipTutorial")] [EnumMember(Value = "cCharTypeSkipTutorial")] SkipTutorial = 2,
-        [XmlEnum("cCharTypePro")] [EnumMember(Value = "cCharTypePro")] Pro = 3
+        [XmlEnum("cCharTypePro")] [EnumMember(Value = "cCharTypePro")] Pro = 3,
+        [XmlEnum("cCharTypeFullLvl40 ")][EnumMember(Value = "cCharTypeFullLvl40 ")] FullLvl40 = 3,
     }
 }
